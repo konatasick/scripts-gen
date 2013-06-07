b31f2a706edc83a3844c24ebca70107817134fdb
ziyuang <ziyuang@gmail.com>
2013-06-07 19:44:35 +0300

    Added shebang.

--- example/command-line.sh
+++ example/command-line.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 ../scripts_gen.py --template template.txt --save-to scripts --param "{'data':'datasets.txt', 'lambda':[`echo 0.{0..9}|sed -E 's/\s+/,/g'`], 'seed':[233,874]}" --format "{data}_lambda={lambda}_seed={seed}.sh" --delete
