3903d1923e79943b3953cbda7e221791e476a1f9
ziyuang <ziyuang@gmail.com>
2013-06-08 11:32:41 +0300

    Splitted into multiline. Strangely Cygwin does not understand this style.

--- example/command-line.sh
+++ example/command-line.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-../scripts_gen.py --template template.txt --save-to scripts --param "{'data':'datasets.txt', 'lambda':[`echo 0.{0..9}|sed -E 's/\s+/,/g'`], 'seed':[233,874]}" --format "{data}_lambda={lambda}_seed={seed}.sh" --delete
+python3 ../scripts_gen.py --template template.txt \
+                          --save-to scripts \
+                          --param "{'data':'datasets.txt', \
+                                    'lambda':[`echo 0.{0..9}|sed -E 's/\s+/,/g'`], \
+                                    'seed':[233,874]}" \
+                          --format "{data}_lambda={lambda}_seed={seed}.sh" \
+                          --delete
